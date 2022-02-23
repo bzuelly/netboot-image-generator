75a41cf04beb873038661d7c08636d74200a0690
Bryan <bzuelly@outlook.com>
2022-02-22 22:19:16 -0500

    working build_iso.sh

--- build_iso.sh
+++ build_iso.sh
@@ -4,23 +4,40 @@ PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 ME=`basename "$0"`
 
-MOUNT_ISO_FOLDER="/mnt/iso"
+MOUNT_ISO_FOLDER="/mnt/iso/"
 EXTRACT_ISO_FOLDER="/tmp/r8"
 NEW_IMAGE_NAME="netboot-RockyV2"
 ORIG_ISO_NAME="Rocky-8-5-x86_64-dvd"
 DOWNLOAD_ISO="Rocky-8.5-x86_64-boot.iso"
-MIRROR=""
+MIRROR="https://rocky.deadbatteries.work/8.5/isos/x86_64/"
 
 # Colors
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 CLS='\033[0m'
 
-wget $MIRROR$DOWNLOAD_ISO
-##mount /tmp/$DOWNLOAD_ISO $MOUNT_ISO_FOLDER
+if [[ -f $DOWNLOAD_ISO  ]]; then
+    echo ""
+    echo "$DOWNLOAD_ISO exists!"
+    mv $DOWNLOAD_ISO /tmp
+    echo ""    
+else
+    wget $MIRROR$DOWNLOAD_ISO
+    mv $DOWNLOAD_ISO /tmp
+fi
 
-#cp -vf $MOUNT_ISO_FOLDER/.??* $EXTRACT_ISO_FOLDER
-##rsync -av --progress $MOUNT_ISO_FOLDER $EXTRACT_ISO_FOLDER
+echo "Mount iso..."
+mount /tmp/$DOWNLOAD_ISO $MOUNT_ISO_FOLDER
+
+echo "Copy iso..."
+if [[ -d /tmp/r8 ]]; then
+    #cp -vf $MOUNT_ISO_FOLDER/.??* $EXTRACT_ISO_FOLDER
+    rsync -av --progress $MOUNT_ISO_FOLDER $EXTRACT_ISO_FOLDER
+else
+    mkdir /tmp/r8
+    #cp -vf $MOUNT_ISO_FOLDER/.??* $EXTRACT_ISO_FOLDER
+    rsync -av --progress $MOUNT_ISO_FOLDER $EXTRACT_ISO_FOLDER
+fi
 
 sed -i '/menu default/d' $EXTRACT_ISO_FOLDER/isolinux/isolinux.cfg
 
@@ -32,7 +49,7 @@ label auto \
   append initrd=initrd.img inst.ks=http://xweb.xlab.lcl/unattend/ks.cfg \
   # end' $EXTRACT_ISO_FOLDER/isolinux/isolinux.cfg
 
-
+echo "build custom iso..."
 mkisofs -o $SCRIPT_PATH/images/$NEW_IMAGE_NAME.iso \
         -b isolinux/isolinux.bin \
 	-c isolinux/boot.cat \
@@ -48,6 +65,8 @@ mkisofs -o $SCRIPT_PATH/images/$NEW_IMAGE_NAME.iso \
 	-V '$ORIG_ISO_NAME' \
 	-T $EXTRACT_ISO_FOLDER
 
+echo "unmount iso..."
 umount $MOUNT_ISO_FOLDER
+echo "cleanup..."
 echo -e "${GREEN}Delete $EXTRACT_ISO_FOLDER${CLS}"
-##rm -rf $EXTRACT_ISO_FOLDER
+rm -rf $EXTRACT_ISO_FOLDER
